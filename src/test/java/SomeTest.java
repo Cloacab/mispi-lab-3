e804ddc09f780729f2eb6843b08547fbd13e87c9
Cloaca_b <martusheved@gmail.com>
2022-05-04 21:02:40 +0300

    Added working test target

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -1,2 +1,8 @@
+import org.junit.Test;
+
 public class SomeTest {
+    @Test
+    public void doSomething() {
+        System.out.println("did something");
+    }
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,48 +1,39 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.*;
 
 public class Tests {
 
-    @BeforeAll
-    static void setup(){
+    @BeforeClass
+    public static void setup() {
         System.out.println("@BeforeAll executed");
     }
 
-    @BeforeEach
-    void setupThis(){
+    @Before
+    public void setupThis() {
         System.out.println("@BeforeEach executed");
     }
 
-    @Tag("DEV")
+    //    @Tag("DEV")
     @Test
-    void testCalcOne()
-    {
+    public void testCalcOne() {
         System.out.println("======TEST ONE EXECUTED=======");
-        Assertions.assertEquals( 4 , 2 + 2);
+        Assert.assertEquals(4, 2 + 2);
     }
 
-    @Tag("PROD")
-    @Disabled
+    //    @Tag("PROD")
+//    @Disabled
     @Test
-    void testCalcTwo()
-    {
+    public void testCalcTwo() {
         System.out.println("======TEST TWO EXECUTED=======");
-        Assertions.assertEquals( 6 , 2 + 4);
+        Assert.assertEquals(6, 2 + 4);
     }
 
-    @AfterEach
-    void tearThis(){
+    @After
+    public void tearThis() {
         System.out.println("@AfterEach executed");
     }
 
-    @AfterAll
-    static void tear(){
+    @AfterClass
+    public static void tear() {
         System.out.println("@AfterAll executed");
     }
 }
